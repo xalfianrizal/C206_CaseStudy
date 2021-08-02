ee06cabee3582f0bab6fa1f644c819443991f537
20026062 <20026062@DESKTOP-FVA8ADO>
2021-08-02 23:23:56 +0800

    Added new method (showAvailability)

--- C206_CaseStudy/src/radioControlCar.java
+++ C206_CaseStudy/src/radioControlCar.java
@@ -5,6 +5,8 @@ public class radioControlCar {
 	private String motorType;
 	private double height;
 	private double length;
+	private boolean isAvailable;
+	
 	
 	public radioControlCar(int assetTag,String name,String material,String motorType,double height,double length) {
 		this.assetTag = assetTag;
@@ -13,6 +15,7 @@ public class radioControlCar {
 		this.motorType = motorType;
 		this.height = height;
 		this.length = length;
+		this.isAvailable = true;
 	}
 	public int getAssetTag() {
 		return assetTag;
@@ -50,9 +53,25 @@ public class radioControlCar {
 	public void setLength(double length) {
 		this.length = length;
 	}
+	public boolean getIsAvailable() {
+		return isAvailable;
+	}
+	public void setIsAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	public String showAvailability(boolean isAvailable) {
+		String avail;
+
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+	}
 	public String display() {
 		String output = "";
-		output += String.format("%-10d %-20s %-20s %-10s %-10.2f %-10.2f %", assetTag,name,material,motorType,height,length);
+		output += String.format("%-10d %-20s %-20s %-10s %-10.2f %-10.2f %-10s %", assetTag,name,material,motorType,height,length,showAvailability(isAvailable));
 		
 		return output;
 	}
