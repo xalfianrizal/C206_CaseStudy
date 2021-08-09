8cf38a33a499a345407b79b93fa9f8cebba247e4
20007756 Alfian <20007756@myrp.edu.sg>
2021-08-09 17:12:00 +0800

    Update on my AppointmentMenu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,85 +1,77 @@
 import java.util.ArrayList;
 
 public class C206_CaseStudy {
-	
-	private static final int QUIT = 6; //Change when done
-	
+
+	private static final int QUIT = 6; // Change when done
+
 	private static final ArrayList<buyer> buyerList = new ArrayList<buyer>();
 	private static final ArrayList<radioControlCar> radioControlCarList = new ArrayList<radioControlCar>();
 	private static final ArrayList<appointment> appointmentList = new ArrayList<appointment>();
 	private static final ArrayList<feedBack> feedBackList = new ArrayList<feedBack>();
 	private static final ArrayList<adminAccount> adminAccountList = new ArrayList<adminAccount>();
-	
+
 	public static void main(String[] args) {
-		
-		buyerList.add(new buyer(1,"Adam",85658898));
-		buyerList.add(new buyer(2,"Bryan",97765571));
-		
-		radioControlCarList.add(new radioControlCar(1,"Monster Truck","aluminum","Brushless",10.47,29.92));
-		radioControlCarList.add(new radioControlCar(2,"Xray","Graphite","T-brace",9.89,28.72));
-		
-		appointmentList.add(new appointment(1,"Charles","23-Aug-2021","3.00PM"));
-		appointmentList.add(new appointment(2,"Dillon","20-Aug-2021","12.30PM"));
-		
-		feedBackList.add(new feedBack(1,"Filip","Some parts are missing"));
-		feedBackList.add(new feedBack(2,"Eunice","Some parts are broken"));
-		
-		adminAccountList.add(new adminAccount(1,"admin","admin123")); // Name/Password Use this to login to system
-		
+
+		buyerList.add(new buyer(1, "Adam", 85658898));
+		buyerList.add(new buyer(2, "Bryan", 97765571));
+
+		radioControlCarList.add(new radioControlCar(1, "Monster Truck", "aluminum", "Brushless", 10.47, 29.92));
+		radioControlCarList.add(new radioControlCar(2, "Xray", "Graphite", "T-brace", 9.89, 28.72));
+
+		appointmentList.add(new appointment(1, "Charles", "23-Aug-2021", "3.00PM"));
+		appointmentList.add(new appointment(2, "Dillon", "20-Aug-2021", "12.30PM"));
+
+		feedBackList.add(new feedBack(1, "Filip", "Some parts are missing"));
+		feedBackList.add(new feedBack(2, "Eunice", "Some parts are broken"));
+
+		adminAccountList.add(new adminAccount(1, "admin", "admin123")); // Name/Password Use this to login to system
+
 		int option = 0;
 		Helper.line(30, "=");
 		System.out.println("Login to access");
 		Helper.line(30, "=");
-		
-		if(valid()) {
+
+		if (valid()) {
 			while (option != QUIT) {
 				menu();
 				option = Helper.readInt("Enter an option > ");
-				if(option == 1) {
+				if (option == 1) {
 					buyerMenu();
-				}
-				else if(option ==2) {
+				} else if (option == 2) {
 					radioControlCarMenu();
-				}
-				else if(option == 3) {
+				} else if (option == 3) {
 					radioControlCarPartsMenu();
-				}
-				else if(option == 4) {
+				} else if (option == 4) {
 					appointmentMenu();
-				}
-				else if(option == 5) {
+				} else if (option == 5) {
 					feedBackMenu();
-				}
-				else if(option == 6) {
+				} else if (option == 6) {
 					System.out.println("Successfully exited!");
-				}
-				else {
+				} else {
 					System.out.println("Invalid option!");
 				}
 			}
-			
-		}
-		else {
+
+		} else {
 			System.out.println("Wrong Login Name or/and Password");
 		}
-		
-		
-		
-		
+
 	}
+
 	public static boolean valid() {
 		String loginName = Helper.readString("Enter Login Name > ");
 		String pass = Helper.readString("Enter Password > ");
-		for(int i = 0;i < adminAccountList.size(); i++) {
-			if(adminAccountList.get(i).getLoginName().equalsIgnoreCase(loginName) && adminAccountList.get(i).getPassword().equals(pass)) {
+		for (int i = 0; i < adminAccountList.size(); i++) {
+			if (adminAccountList.get(i).getLoginName().equalsIgnoreCase(loginName)
+					&& adminAccountList.get(i).getPassword().equals(pass)) {
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
 		}
 		return false;
 	}
+
 	public static void menu() {
 		Helper.line(30, "=");
 		System.out.println("Welcome to the Application");
@@ -91,16 +83,19 @@ public class C206_CaseStudy {
 		System.out.println("5. Feedback Menu");
 		System.out.println("6. Quit");
 	}
+
 	public static void buyerMenu() {
-		
+
 	}
+
 	public static void radioControlCarMenu() {
 	}
-	
+
 	public static void radioControlCarPartsMenu() {
-		
+
 	}
-	//Appointment Side
+
+	// Appointment Side
 	public static void showAppointmentMenu() {
 		Helper.line(30, "=");
 		System.out.println("APPOINTMENT MENU");
@@ -110,47 +105,91 @@ public class C206_CaseStudy {
 		System.out.println("3. View appointment");
 		System.out.println("4. Exit\n");
 	}
+
+	public static boolean checkAppointmentIDExists(int appID) {
+		boolean check = false;
+		for (int i = 0; i < appointmentList.size(); i++) {
+			if (appointmentList.get(i).getAppointmentID() == appID) {
+				check = true;
+			}
+		}
+		return check;
+
+	}
+
+	public static boolean checkAppointmentDateTimeExists(String Date, String Time) {
+		boolean check = false;
+		for (int i = 0; i < appointmentList.size(); i++) {
+			if (appointmentList.get(i).getDate().equals(Date) && appointmentList.get(i).getTime().equals(Time)) {
+				check = true;
+			}
+		}
+		return check;
+
+	}
+
 	public static void addAppointment() {
 		Helper.line(30, "-");
 		System.out.println("ADD APPOINTMENT");
 		Helper.line(30, "-");
-		
+
 		int appointmentID = Helper.readInt("Enter an Appointment ID > ");
 		String customerName = Helper.readString("Enter the Customers Name > ");
 		String date = Helper.readString("Enter the date (DD-MMM-YYYY) > ");
 		String time = Helper.readString("Enter the time (TT:TT(AM/PM) > ");
+
+		if (customerName.isEmpty() && date.isEmpty() && time.isEmpty()) {
+			System.out.println("Please do not leave the fields empty! Add failed!");
+		} 
 		
-		appointmentList.add(new appointment(appointmentID,customerName,date,time));
-		System.out.println("\nSuccess!");
+		else if (checkAppointmentDateTimeExists(date, time) == true) {
+			System.out.println("The date and timeslot  has already been taken! Add failed!");
+		}
+
+		else if (checkAppointmentIDExists(appointmentID) == true) {
+			System.out.println("Appointment ID already exists! Add failed!");
+		}
+
+		else {
+			appointmentList.add(new appointment(appointmentID, customerName, date, time));
+			System.out.println("\nThe appointment has been added successfully!");
+		}
 	}
 
-	public static void deleteAppointment() {
-		
-	}
-	
-	public static void viewAppointment() {
-		
-	}
 	public static void appointmentMenu() {
 		int option = 0;
 		while (option != 4) {
 			showAppointmentMenu();
 			option = Helper.readInt("Enter an option > ");
-			
+
 			if (option == 1) {
 				addAppointment();
 			}
-			
+
+			else if (option == 4) {
+				System.out.println("Returning to application menu...\n");
+			}
+
 			else {
-			System.out.println("Invalid option! Please try again!\n");
+				System.out.println("Invalid option! Please try again!\n");
+			}
+
 		}
-			
-		}
-		
+
 	}
-	
+
+	//
+
+	public static void deleteAppointment() {
+
+	}
+
+	public static void viewAppointment() {
+
+	}
+
 	public static void feedBackMenu() {
-		
+
 	}
 
 }
